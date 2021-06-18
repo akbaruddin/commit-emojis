8435cb8fb6ec588cee5ff68a69967963a0678bdf
Akbar <akbaruddin@live.in>
2021-06-18 09:31:27 +0530

    update metas 🔍

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HeadProvider } from 'react-head';
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HeadProvider>
+      <App />
+    </HeadProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
