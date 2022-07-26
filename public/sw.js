7baec093d60cad889fdf56c20b93231db715be6d
akbaruddin <akbaruddin@live.in>
2022-07-26 14:59:31 +0530

    fix: loading events ⚡️

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ const cacheName = "Commit-Emojis";
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(["/", "/site.webmanifest"]);
+      return cache.addAll(["/", "/site.webmanifest", "https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css", "https://cdn.jsdelivr.net/npm/toastify-js"]);
     })
   );
 });
