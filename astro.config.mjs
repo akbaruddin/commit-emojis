baccf24bcf07d004794aa477a873ba0497decc0d
akbaruddin <akbaruddin@live.in>
2022-07-26 14:44:33 +0530

    improve notification ⚗
    - cache
    - seo
    - notification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://commit-emojis.vercel.app/',
+  integrations: [tailwind(), sitemap()]
 });
